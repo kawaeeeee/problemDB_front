e8400d7a39c26b8dc08c2ddc0fc96295748200bf
kawaeeeee <u639748d@ecs.osaka-u.ac.jp>
2024-08-18 13:57:14 +0900

    ルータを使わないようにした

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import FileUpload from './FileUpload';
 import UnitForm from './UnitForm';
 import UnitList from './UnitList';
 import apiClient from './api';
-import AddProblemForm from './AddProblemForm';
 import ProblemList from './ProblemList';
 import MenuBar from './MenuBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddProblemButton from './AddProblemButton';
 
 const modalStyle = {
@@ -27,47 +25,45 @@ const modalStyle = {
 
 const App = () => {
     const [openunitf,setOpenunitf] = useState(false);//
+    const [screen, setScreen] = useState(1);
     const handleOpenunitf = () => setOpenunitf(true);//単元フォームモーダルを開くための関数
     const handleCloseunitf = () => setOpenunitf(false);
 
 
     return (
         <div className='App'>
-            <Router>
-                <MenuBar></MenuBar>
-                <Routes>
-                    <Route path = "/" element={
-                        <div>
-                            <ProblemList isPrintable={true}/>
-                        </div>
-                    } />
-                    <Route path="editunit" element={
-                        <div>
-                            <Button onClick={handleOpenunitf}>単元を追加</Button>
-                            <UnitList></UnitList>
-                            <Modal
-                                open={openunitf}
-                                onClose={handleCloseunitf}
-                                aria-labelledby="modal-modal-title"
-                                aria-describedby="modal-modal-description"
-                            >
-                                <Box sx={modalStyle}>
-                                    <UnitForm></UnitForm>
-                                </Box>
-                            </Modal>
-                        </div>
-                    } />
-                    <Route path="editproblem" element={
-                        <div>
-                            <AddProblemButton />
-                            <ProblemList isEditable={true}/>
-                        </div>
-                    } />
-
-                </Routes>
-            </Router>
-            
-            
+            <MenuBar 
+                onHomeClick={() => setScreen(1)} 
+                onEditUnitClick={() => setScreen(2)}
+                onEditProblemClick={() => setScreen(3)}
+            />
+            {screen === 1 && (
+                <div>
+                    <ProblemList isPrintable={true} />
+                </div>
+            )}
+            {screen === 2 && (
+                <div>
+                    <Button onClick={handleOpenunitf}>単元を追加</Button>
+                    <UnitList />
+                    <Modal
+                        open={openunitf}
+                        onClose={handleCloseunitf}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={modalStyle}>
+                            <UnitForm />
+                        </Box>
+                    </Modal>
+                </div>
+            )}
+            {screen === 3 && (
+                <div>
+                    <AddProblemButton />
+                    <ProblemList isEditable={true} />
+                </div>
+            )}
         </div>
     );
 };
--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom'; // react-router-domのLinkを使用する
+//import { Link } from 'react-router-dom'; // react-router-domのLinkを使用する
 
-const MenuBar = () => {
+const MenuBar = ({ onHomeClick, onEditUnitClick, onEditProblemClick}) => {
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     問題データベース
                 </Typography>
-                <Button component={Link} to="/" color="inherit">ホーム</Button>
+                {/* <Button component={Link} to="/" color="inherit">ホーム</Button>
                 <Button component={Link} to="/editunit" color="inherit">単元の編集</Button>
-                <Button component={Link} to="/editproblem" color="inherit">問題の編集</Button>
+                <Button component={Link} to="/editproblem" color="inherit">問題の編集</Button> */}
+                <Button onClick={onHomeClick} color='inherit'>ホーム</Button>
+                <Button onClick={onEditUnitClick} color='inherit'>単元の編集</Button>
+                <Button onClick={onEditProblemClick} color='inherit'>問題の編集</Button>
             </Toolbar>
         </AppBar>
     );
